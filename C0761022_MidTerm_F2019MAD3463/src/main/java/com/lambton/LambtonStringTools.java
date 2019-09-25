e72fbe54c6c6871b530dbeb88c20897368996330
The Shimmering World <sanjeevgupta9223@gmail.com>
2019-09-25 15:35:50 -0700

    implemented the first reverse function

---
+++ C0761022_MidTerm_F2019MAD3463/src/main/java/com/lambton/LambtonStringTools.java
@@ -0,0 +1,27 @@
+package com.lambton;
+
+public class LambtonStringTools
+{
+    public void reverse(String s)
+    {
+        int length = s.length();
+        String reverse="";
+
+        for ( int i = length - 1 ; i >= 0 ; i-- )
+            reverse = reverse + s.charAt(i);
+
+        System.out.println("Reversed String = "+reverse);
+    }
+
+  
+
+    public void mostFrequent(String s)
+    {
+
+    }
+
+    public void replaceSubString(String s1, String s2, String s3)
+    {
+
+    }
+}
---
+++ C0761022_MidTerm_F2019MAD3463/src/main/java/com/lambton/MainClass.java
@@ -0,0 +1,12 @@
+package com.lambton;
+
+public class MainClass {
+
+    public static void main(String args[]) {
+
+        LambtonStringTools lambtonStringTools = new LambtonStringTools();
+        lambtonStringTools.reverse("Lambton");
+        lambtonStringTools.binaryToDecimal("1000");
+        lambtonStringTools.initials("jean luc picard");
+    }
+}
